6580772b28f94054b87aef1864bee82c4a7e119c
mahmoudEwiis <mahmoud.ewis97@gmail.com>
2025-05-17 23:18:51 +0300

    Update environemt File

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
+  production: true, 
   api: 'https://api.escuelajs.co/api/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'https://api.escuelajs.co/api/',
+  api: 'https://api.escuelajs.co/api/', 
 };
 
 /*
