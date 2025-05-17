494196758b6df271a92f5c658460787905185ab0
mahmoudEwiis <mahmoud.ewis97@gmail.com>
2025-05-17 23:21:18 +0300

    Updating Style

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,7 +18,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }); 
 
   it(`should have as title 'Ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
